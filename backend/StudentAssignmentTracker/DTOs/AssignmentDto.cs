f79086a6fb2a5dd5df9522f8254b174bbd332695
Ansh Pal <Pansh2004@gmail.com>
2026-03-24 22:53:03 +0530

    Created task APIs

---
+++ backend/StudentAssignmentTracker/DTOs/AssignmentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudentAssignmentTracker.DTOs
+{
+    public class AssignmentDto
+    {
+        public string Title { get; set; }
+
+        public string Subject { get; set; }
+
+        public DateTime Deadline { get; set; }
+    }
+}
---
+++ backend/StudentAssignmentTracker/Models/Assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StudentAssignmentTracker.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Subject { get; set; }
+
+        public DateTime Deadline { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+    }
+}
